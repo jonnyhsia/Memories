46a080ee675342621722b0dcb7f52be00ee105ec
jonnyhsia <jianhaoxia@gmail.com>
2019-05-17 18:17:35 +0800

    工具栏UI, ImageToggle

--- app/src/main/java/com/jonnyhsia/memories/page/compose/ComposeFragment.kt
+++ app/src/main/java/com/jonnyhsia/memories/page/compose/ComposeFragment.kt
@@ -4,25 +4,32 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.DecelerateInterpolator
 import android.view.animation.Interpolator
-import android.widget.FrameLayout
-import androidx.appcompat.widget.TooltipCompat
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.transaction
+import androidx.recyclerview.widget.RecyclerView
+import com.arch.jonnyhsia.ui.ImageToggle
 import com.arch.jonnyhsia.ui.ext.tooltipTextCompat
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.jonnyhsia.appcore.component.BaseFragment
+import com.jonnyhsia.appcore.component.xsubscribe
+import com.jonnyhsia.appcore.ext.Colors
 import com.jonnyhsia.appcore.ext.click
+import com.jonnyhsia.appcore.ext.dp
 import com.jonnyhsia.memories.R
 import com.jonnyhsia.memories.application
 import com.jonnyhsia.memories.page.compose.format.FormatFragment
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.compose_fragment.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
 
 private const val TOOL_AREA_ANIM_DURATION = 360L
 private val TOOL_AREA_INTERPOLATOR: Interpolator = DecelerateInterpolator(2.5f)
 
-private val TOOL_AREA_TRANSLATION_Y = application.resources.getDimensionPixelSize(R.dimen.tool_area_translation_y).toFloat()
+private val TOOL_AREA_TRANSLATION_Y = application.resources.getDimensionPixelSize(R.dimen.tool_area_height).toFloat()
 
 class ComposeFragment : BaseFragment<ComposeViewModel>() {
 
@@ -31,17 +38,16 @@ class ComposeFragment : BaseFragment<ComposeViewModel>() {
 
     override val vm: ComposeViewModel by viewModel()
 
-    private var isToolAreaEnable = false
     private var toolAreaSelectedIndex = -1
-    private var toolsBehavior: BottomSheetBehavior<FrameLayout> by Delegates.notNull()
+
+    private var behavior: BottomSheetBehavior<RecyclerView> by Delegates.notNull()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        toolsBehavior = BottomSheetBehavior.from(toolAreaContainer)
-        toolsBehavior.peekHeight = TOOL_AREA_TRANSLATION_Y.toInt()
-
-        TooltipCompat.setTooltipText(btnGallery, "图片")
+        behavior = BottomSheetBehavior.from(recyclerQuickText)
+        behavior.peekHeight = 122.dp
+        recyclerQuickText.setOnClickListener { }
 
         btnBack.click(vm) {
             back()
@@ -53,73 +59,119 @@ class ComposeFragment : BaseFragment<ComposeViewModel>() {
         }
 
         btnFormat.tooltipTextCompat = "格式化工具"
-        btnFormat.click(vm) {
-            enableToolbarArea(1)
-            showFormatTools()
-        }
+        btnFormat.setOnCheckedChangeListener(object : ImageToggle.OnCheckedChangeListener {
+            override fun onCheckedChanged(view: ImageToggle, checked: Boolean) {
+                enableToolbarArea(checked)
+                if (checked) {
+                    btnQuickText.isChecked = false
+                    showFormatTools()
+                }
+            }
+        })
 
         btnMention.tooltipTextCompat = "提及你的朋友"
         btnMention.click(vm) {
         }
 
         btnQuickText.tooltipTextCompat = "快捷输入"
-        btnQuickText.click(vm) {
-            enableToolbarArea(3)
-        }
+        btnQuickText.setOnCheckedChangeListener(object : ImageToggle.OnCheckedChangeListener {
+            override fun onCheckedChanged(view: ImageToggle, checked: Boolean) {
+                quickInputArea.isVisible = checked
+                if (checked) {
+                    btnFormat.isChecked = false
+                }
+
+                recyclerQuickText.isVisible = true
+                recyclerQuickText.animate()
+                        .translationY(if (checked) 0f else 122f.dp)
+                        .apply {
+                            if (!checked) {
+                                withEndAction { recyclerQuickText.isVisible = false }
+                            }
+                        }
+                        .start()
+            }
+        })
+//        btnQuickText.click(vm) {
+//            // enableToolbarArea(3)
+//            quickInputArea.isVisible = true
+//            recyclerQuickText.isVisible = true
+//            recyclerQuickText.animate()
+//                    .translationY(0f)
+//                    .start()
+//        }
 
         btnAssistant.tooltipTextCompat = "Potato Assistant (Alpha)"
         btnAssistant.click(vm) {
         }
+
+        swInformation.setFactory {
+            TextView(requireContext()).apply {
+                textAlignment = View.TEXT_ALIGNMENT_TEXT_END
+                textSize = 13f
+                setTextColor(Colors(R.color.textColorCaption))
+                letterSpacing = 0.03f
+            }
+        }
+
+        Observable.intervalRange(0, 10, 2000L, 3000L, TimeUnit.MILLISECONDS)
+                .map {
+                    if (it % 2 == 0L) "已成功自动保存" else ""
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .xsubscribe(vm, onNext = {
+                    swInformation.setText(it)
+                })
     }
 
     private fun showFormatTools() {
         childFragmentManager.transaction {
             val fragment = childFragmentManager.findFragmentByTag("format")
                     ?: FormatFragment()
-            if (fragment.isVisible) {
+            if (fragment.isAdded) {
                 return
             }
-            replace(R.id.toolAreaContainer, fragment)
+            replace(R.id.toolAreaContainer, fragment, "format")
         }
     }
 
-    private fun enableToolbarArea(index: Int) {
-        if (toolAreaSelectedIndex == index && isToolAreaEnable) {
-            isToolAreaEnable = false
-            toolAreaSelectedIndex = index
+    private fun enableToolbarArea(enable: Boolean) {
+        if (!enable) {
             // 还原
-            toolbar.animate()
-                    .translationY(0f)
-                    .setInterpolator(TOOL_AREA_INTERPOLATOR)
-                    .setDuration(TOOL_AREA_ANIM_DURATION)
-                    .start()
-
-            toolsBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            toolAreaContainer.animate()
-                    .translationY(TOOL_AREA_TRANSLATION_Y)
-                    .setInterpolator(TOOL_AREA_INTERPOLATOR)
-                    .setDuration(TOOL_AREA_ANIM_DURATION)
-                    .withEndAction { toolAreaContainer.visibility = View.INVISIBLE }
-                    .start()
+            toolAreaContainer.isVisible = false
+//            isToolAreaEnable = false
+//            toolAreaSelectedIndex = index
+//            toolbar.animate()
+//                    .translationY(0f)
+//                    .setInterpolator(TOOL_AREA_INTERPOLATOR)
+//                    .setDuration(TOOL_AREA_ANIM_DURATION)
+//                    .start()
+//
+//            toolAreaContainer.animate()
+//                    .translationY(TOOL_AREA_TRANSLATION_Y)
+//                    .setInterpolator(TOOL_AREA_INTERPOLATOR)
+//                    .setDuration(TOOL_AREA_ANIM_DURATION)
+//                    .withEndAction { toolAreaContainer.visibility = View.INVISIBLE }
+//                    .start()
             return
         }
 
-        if (toolAreaSelectedIndex != index || !isToolAreaEnable) {
-            toolAreaSelectedIndex = index
-            isToolAreaEnable = true
+        if (enable) {
             // 展开
-            toolbar.animate()
-                    .translationY(-TOOL_AREA_TRANSLATION_Y)
-                    .setInterpolator(TOOL_AREA_INTERPOLATOR)
-                    .setDuration(TOOL_AREA_ANIM_DURATION)
-                    .start()
-
             toolAreaContainer.isVisible = true
-            toolAreaContainer.animate()
-                    .translationY(0f)
-                    .setInterpolator(TOOL_AREA_INTERPOLATOR)
-                    .setDuration(TOOL_AREA_ANIM_DURATION)
-                    .start()
+//            isToolAreaEnable = true
+//            toolbar.animate()
+//                    .translationY(-TOOL_AREA_TRANSLATION_Y)
+//                    .setInterpolator(TOOL_AREA_INTERPOLATOR)
+//                    .setDuration(TOOL_AREA_ANIM_DURATION)
+//                    .start()
+//
+//            toolAreaContainer.isVisible = true
+//            toolAreaContainer.animate()
+//                    .translationY(0f)
+//                    .setInterpolator(TOOL_AREA_INTERPOLATOR)
+//                    .setDuration(TOOL_AREA_ANIM_DURATION)
+//                    .start()
         }
     }
 }
---
+++ ui/src/main/java/com/arch/jonnyhsia/ui/ImageToggle.kt
@@ -0,0 +1,67 @@
+package com.arch.jonnyhsia.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Checkable
+import androidx.appcompat.widget.AppCompatImageView
+
+private val DRAWABLE_STATE_CHECKED = intArrayOf(android.R.attr.state_checked)
+
+class ImageToggle @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr), Checkable {
+
+    private var isChecked: Boolean
+    private var onCheckedChangeListener: OnCheckedChangeListener? = null
+
+    interface OnCheckedChangeListener {
+
+        fun onCheckedChanged(view: ImageToggle, checked: Boolean)
+    }
+
+    init {
+        isClickable = true
+        isFocusable = true
+        isDuplicateParentStateEnabled = true
+        val a = getContext().obtainStyledAttributes(attrs, R.styleable.ImageToggle)
+        isChecked = a.getBoolean(R.styleable.ImageToggle_android_checked, false)
+        a.recycle()
+    }
+
+    override fun isChecked(): Boolean {
+        return isChecked
+    }
+
+    override fun toggle() {
+        setChecked(!isChecked)
+    }
+
+    fun setOnCheckedChangeListener(listener: OnCheckedChangeListener?) {
+        onCheckedChangeListener = listener
+    }
+
+    override fun performClick(): Boolean {
+        val b = super.performClick()
+        toggle()
+        return b
+    }
+
+    override fun setChecked(checked: Boolean) {
+        if (checked == isChecked) {
+            return
+        }
+
+        isChecked = checked
+        onCheckedChangeListener?.onCheckedChanged(this, isChecked)
+        refreshDrawableState()
+    }
+
+    override fun onCreateDrawableState(extraSpace: Int): IntArray {
+        return if (isChecked) {
+            View.mergeDrawableStates(super.onCreateDrawableState(extraSpace + DRAWABLE_STATE_CHECKED.size), DRAWABLE_STATE_CHECKED)
+        } else {
+            super.onCreateDrawableState(extraSpace)
+        }
+    }
+}
